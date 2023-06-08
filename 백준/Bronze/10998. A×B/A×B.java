8576b9b1570711ff27c2ed043b4277bf17423317
ParkUiel <128464859+parkuiery@users.noreply.github.com>
2023-06-08 17:14:52 +0900

    [Bronze V] Title: A×B, Time: 216 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n1 =0;
+        int n2=0;
+
+        Scanner sc = new Scanner(System.in);
+
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+
+        System.out.print(n1 * n2);
+
+    }
+}
