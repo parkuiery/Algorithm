f38026b1b20ce449be2149dba56a0d32a378d933
ParkUiel <128464859+parkuiery@users.noreply.github.com>
2023-06-08 17:17:47 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n1 =0;
+        int n2=0;
+
+        Scanner sc = new Scanner(System.in);
+
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+
+        System.out.println(n1 + n2);
+        System.out.println(n1 -  n2);
+        System.out.println(n1 * n2);
+        System.out.println(n1 / n2);
+        System.out.println(n1 % n2);
+
+    }
+}
