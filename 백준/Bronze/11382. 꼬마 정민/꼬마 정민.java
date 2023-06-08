2932dcdec6d07f2616c7e34cd928b476d63ef22c
ParkUiel <128464859+parkuiery@users.noreply.github.com>
2023-06-08 22:43:41 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        long n1 = 0;
+        long n2 = 0;
+        long n3 = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        n1 = sc.nextLong();
+        n2 = sc.nextLong();
+        n3 = sc.nextLong();
+
+        System.out.println(n1+n2+n3);
+    }
+}
