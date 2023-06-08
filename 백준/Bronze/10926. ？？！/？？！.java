e614ac174d0b8b3219a93920dc7ec13c556f56af
ParkUiel <128464859+parkuiery@users.noreply.github.com>
2023-06-08 17:21:59 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String s = null;
+
+        Scanner sc = new Scanner(System.in);
+
+        s = sc.next();
+        System.out.print(s + "??!");
+
+
+    }
+}
