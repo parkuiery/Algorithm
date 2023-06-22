781ab90273827461578e873383138e9fe23733ff
ParkUiel <128464859+parkuiery@users.noreply.github.com>
2023-06-22 21:17:08 +0900

    [Bronze V] Title: 개수 세기, Time: 224 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+        int c=0;
+        int[] arg = new int[100];
+        Scanner scanner = new Scanner(System.in);
+
+        b = scanner.nextInt();
+        for(int i=0;i<b;i++){
+            arg[i] = scanner.nextInt();
+        }
+        a = scanner.nextInt();
+        for(int i=0;i<b;i++){
+            if(arg[i]==a){
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
